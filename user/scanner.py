ff5685800783c169989734e88333e05031bc013b
rattle99 <24495512+rattle99@users.noreply.github.com>
2021-12-13 12:38:55 +0530

    Add function docstrings

--- user/scanner.py
+++ user/scanner.py
@@ -5,6 +5,15 @@ from pyxirr import xirr
 
 
 def getLatest(URL_ALL='https://www.amfiindia.com/spages/NAVAll.txt'):
+    """Returns a dataframe with the latest NAV of all mutual funds of the current day.
+
+    Args:
+        URL_ALL (str, optional): URL to fetch data from. Defaults to 'https://www.amfiindia.com/spages/NAVAll.txt'.
+
+    Returns:
+        obj: A pandas dataframe.
+    """
+
     dt = pd.read_csv(URL_ALL)
     dt = dt.rename(columns={
                    'Scheme Code;ISIN Div Payout/ ISIN Growth;ISIN Div Reinvestment;Scheme Name;Net Asset Value;Date': 1})
@@ -17,6 +26,16 @@ def getLatest(URL_ALL='https://www.amfiindia.com/spages/NAVAll.txt'):
 
 
 def getFundXirr(df, df_All):
+    """Calculate XIRR for investments made into a particular fund.
+
+    Args:
+        df (obj): Pandas dataframe of transactions for a particular fund.
+        df_All (obj): Pandas dataframe of latest NAVs of all funds for the day.
+
+    Returns:
+        float: Calculated XIRR value.
+    """
+
     amfi_code = str(df.amfi.iloc[0])
     df_fund = df_All.loc[df_All['amfi'] == amfi_code]
     b1 = df.iloc[-1].loc['balance'].copy()
@@ -30,6 +49,17 @@ def getFundXirr(df, df_All):
 
 
 def getNameAndPeriod(filename, password):
+    """Fetch name of user and the statement time period from CAS file.
+
+    Args:
+        filename (str): File path for CAS file to read from.
+        password (str): Password to open CAS file.
+
+    Returns:
+        str: Name of user.
+        dict: Dictionary with from and to date for account statement period.
+    """
+
     data = casparser.read_cas_pdf(filename, password, output="json")
     d = json.loads(data)
     name = d['investor_info']['name']
@@ -38,6 +68,19 @@ def getNameAndPeriod(filename, password):
 
 
 def getData(filename, password):
+    """Fetches and calculate fund data for user in terms of investments, returns generated, XIRR, etc. 
+    Uses helper functions inside.
+
+    Args:
+        filename (str): File path for CAS file to read from.
+        password (str): Password to open CAS file.
+
+    Returns:
+        list: A list of dictionaries with data for each individual fund for each element in list.
+        str: Name of user.
+        dict: Dictionary with from and to date for account statement period.
+    """
+
     data = casparser.read_cas_pdf(filename, password, output="csv")
     f = open("temp.csv", "w")
     f.write(data)
@@ -83,6 +126,21 @@ def getData(filename, password):
 
 
 def finalParser(filename, password):
+    """Fetches and calculate fund data for user in terms of investments, returns generated, XIRR, etc. 
+    Uses helper functions inside.
+
+    Args:
+        filename (str): File path for CAS file to read from.
+        password (str): Password to open CAS file.
+
+    Returns:
+        list: A list of dictionaries with data for each individual fund for each element in list.
+        str: Name of user.
+        dict: Dictionary with from and to date for account statement period.
+        dict: Dictionary for total portfolio summary with metrics for investment amount, current 
+        amount, PnL, XIRR, etc.
+    """
+
     userData, name, statementPeriod = getData(filename, password)
     investment = 0
     current = 0
